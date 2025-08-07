f04fe24405ae7ff250cdd4a611ed4e52aae23dd6
Andres Calandri <andresmcalandri@gmail.com>
2025-08-07 22:53:06 +0200

    Fixed build script and package.json

--- MAG_GameLibraries/BuildScripts/build.sh
+++ MAG_GameLibraries/BuildScripts/build.sh
@@ -1,14 +1,13 @@
 cd ..
 
 rm -rf Builds
-mkdir Builds
-mkdir Builds/temp
+mkdir -p Builds/package/Runtime
 
-cp ./BuildScripts/maggamelibraries.asmdef Builds/temp/
-cp ./BuildScripts/package.json Builds/temp/
+cp ./BuildScripts/maggamelibraries.asmdef Builds/package/
+cp ./BuildScripts/package.json Builds/package/
 
 dotnet build MAG_GameLibraries.csproj -c Release
-cp ./bin/Release/netstandard2.1/MAG_GameLibraries.dll Builds/temp/
+cp ./bin/Release/netstandard2.1/MAG_GameLibraries.dll Builds/package/Runtime/
 
-# Create tar package
-tar -czf ./Builds/maggamelibraries-v0.0.1.tgz ./Builds/temp/
+cd Builds/
+tar -czf maggamelibraries-v0.0.1.tgz -C package .
