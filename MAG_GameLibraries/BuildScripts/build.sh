20c48d4a1570bc0990029468186f25a55bbf84b3
Andres Calandri <andresmcalandri@gmail.com>
2025-08-07 23:09:42 +0200

    Fixed missing dependencies on package

--- MAG_GameLibraries/BuildScripts/build.sh
+++ MAG_GameLibraries/BuildScripts/build.sh
@@ -6,8 +6,10 @@ mkdir -p Builds/package/Runtime
 cp ./BuildScripts/maggamelibraries.asmdef Builds/package/
 cp ./BuildScripts/package.json Builds/package/
 
-dotnet build MAG_GameLibraries.csproj -c Release
+dotnet publish MAG_GameLibraries.csproj -c Release -f netstandard2.1
 cp ./bin/Release/netstandard2.1/MAG_GameLibraries.dll Builds/package/Runtime/
+cp ./bin/Release/netstandard2.1/publish/Microsoft.Extensions.DependencyInjection.dll Builds/package/Runtime/
+cp ./bin/Release/netstandard2.1/publish/Microsoft.Extensions.DependencyInjection.Abstractions.dll Builds/package/Runtime/
 
 cd Builds/
 tar -czf maggamelibraries-v0.0.1.tgz -C package .
