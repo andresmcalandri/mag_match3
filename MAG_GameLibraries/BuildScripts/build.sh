ba77d3dd9f7de02532dead1da02569ca31fde101
Andres Calandri <andresmcalandri@gmail.com>
2025-08-08 16:09:49 +0200

    Fixed tarball not being properly generated and missing meta files

--- MAG_GameLibraries/BuildScripts/build.sh
+++ MAG_GameLibraries/BuildScripts/build.sh
@@ -5,6 +5,7 @@ mkdir -p Builds/package/Runtime
 
 cp ./BuildScripts/maggamelibraries.asmdef Builds/package/
 cp ./BuildScripts/package.json Builds/package/
+cp -r ./BuildScripts/MetaFiles/* Builds/Package/
 
 dotnet publish MAG_GameLibraries.csproj -c Release -f netstandard2.1
 cp ./bin/Release/netstandard2.1/MAG_GameLibraries.dll Builds/package/Runtime/
@@ -12,4 +13,4 @@ cp ./bin/Release/netstandard2.1/publish/Microsoft.Extensions.DependencyInjection
 cp ./bin/Release/netstandard2.1/publish/Microsoft.Extensions.DependencyInjection.Abstractions.dll Builds/package/Runtime/
 
 cd Builds/
-tar -czf maggamelibraries-v0.0.1.tgz -C package .
+tar -czf maggamelibraries-v0.0.1.tar.gz *
